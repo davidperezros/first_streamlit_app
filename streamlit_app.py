a41b7cad3907e9aff14b94c8728170070b970c06
davidperezros <114222795+davidperezros@users.noreply.github.com>
2023-03-31 12:03:48 +0200

    Update streamlit_app.py
    correct errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text( '🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocadro Toast')
