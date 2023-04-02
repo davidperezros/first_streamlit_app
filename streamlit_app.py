aa234db9af2ddb2039f932edd66d5d5a2fa9a02b
davidperezros <114222795+davidperezros@users.noreply.github.com>
2023-04-02 23:24:00 +0200

    Update streamlit_app.py
    error in kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 
 # write your own comment -what does the next line do? 
