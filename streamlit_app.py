066d43690eca0d884bf0199e0fef9480a5553217
davidperezros <114222795+davidperezros@users.noreply.github.com>
2023-04-03 09:45:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,3 +62,4 @@ streamlit.header("Fruityvice Fruit Advdice!")
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered now', add_my_fruit)
 
+my_cur.execute("insert into  fruit_load_list values ('from streamlit')")
