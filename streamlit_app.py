e3e372f0e73420e26fc08f0f2cecb7a136e37b11
davidperezros <114222795+davidperezros@users.noreply.github.com>
2023-04-03 09:32:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,5 +60,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"add_my_fruit")
+fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+"add_my_fruit")
 
