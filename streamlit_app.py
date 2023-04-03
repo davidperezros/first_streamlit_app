78e3155afe008f6da2b5f1a8c2739d3930d06ec3
davidperezros <114222795+davidperezros@users.noreply.github.com>
2023-04-03 09:41:05 +0200

    Update streamlit_app.py
    apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,9 @@ streamlit.dataframe(my_data_rows)
 
 
 
+
+# New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advdice!")
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','apple')
+streamlit.write('The user entered now', add_my_fruit)
+
